d4884dea26423575b199d2f37bc073a7d5a8f325
Klementina424 <58881767+Klementina424@users.noreply.github.com>
2020-11-17 00:13:49 +0300

    Add files via upload

--- UnitTest2.cpp
+++ UnitTest2.cpp
@@ -13,6 +13,7 @@ namespace UnitTest2
 		
 		int arr[10] = { 1,2,0,10,7,3,9,8,11,4 };
 		int arrsort[10] = { 0,1,2,3,4,5,6,7,8,9 };
+		char arrcountsort[10] = { 'a','s','b','c','f','e','z','g','h','k' };
 		
 		TEST_METHOD(BinarySearchTest)
 		{
@@ -40,5 +41,10 @@ namespace UnitTest2
 			Assert::IsTrue(arr[1] == 1, L"Assert 2");
 		}
 		
+		TEST_METHOD(CountingSortTest)
+		{
+			CountingSort(arrcountsort, 10);
+			Assert::IsTrue(arrcountsort[0] == 'a', L"Assert 1");
+			Assert::IsTrue(arrcountsort[9] == 'z', L"Assert 2");
 	};
 }
